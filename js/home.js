59dd09574b30811138ab7eb60b2bc540417305cf
Lukas Santos Venancio <lukassvenancio022@gmail.com>
2022-02-07 23:01:57 -0300

    Home completed.

---
+++ js/home.js
@@ -0,0 +1,7 @@
+function abrir() {
+    document.querySelector(".menu-burger-container").classList.toggle("active");
+  }
+  function fechar() {
+    document.querySelector(".menu-burger-container").classList.remove("active");
+  }
+  document.querySelector(".menu-burger-container").addEventListener("click", abrir);
